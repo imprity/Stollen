c691a323cf411da05e472fd4ccbadbcc8ab5f1ff
joe <imprity041@gmail.com>
2023-05-29 04:26:03 +0900

    implemented json printing feature in bread script

--- bread.js
+++ bread.js
@@ -34,12 +34,44 @@ var process = require("process");
 var args = process.argv.slice(2);
 if (args.length === 0 || args[0] === '-h') {
     console.log('parses the files passed to arguments and prints tree');
-    console.log('-h : prints this message');
+    console.log('-h     : prints this message');
+    console.log('--json : print as json');
     process.exit(0);
 }
+var ItemWithoutPrent = /** @class */ (function () {
+    function ItemWithoutPrent(item) {
+        var e_2, _a;
+        this.attributes = [];
+        this.body = [];
+        this.attributes = item.attributes;
+        try {
+            for (var _b = __values(item.body), _c = _b.next(); !_c.done; _c = _b.next()) {
+                var child = _c.value;
+                if (typeof child === 'string') {
+                    this.body.push(child);
+                }
+                else {
+                    var cloneChild = new ItemWithoutPrent(child);
+                    this.body.push(cloneChild);
+                }
+            }
+        }
+        catch (e_2_1) { e_2 = { error: e_2_1 }; }
+        finally {
+            try {
+                if (_c && !_c.done && (_a = _b.return)) _a.call(_b);
+            }
+            finally { if (e_2) throw e_2.error; }
+        }
+    }
+    return ItemWithoutPrent;
+}());
 try {
     for (var args_1 = __values(args), args_1_1 = args_1.next(); !args_1_1.done; args_1_1 = args_1.next()) {
         var filePath = args_1_1.value;
+        if (filePath === '--json' || filePath === '-h') {
+            continue;
+        }
         try {
             var content = fs.readFileSync(filePath, 'utf-8');
             var tokenizer = new st.Tokenizer(content, filePath);
@@ -49,11 +81,17 @@ try {
                 console.error(errorMsg);
             }
             else {
-                console.log(st.prettyPrint(root, Boolean(process.stdout.isTTY)));
+                if (args.includes('--json')) {
+                    var rootClone = new ItemWithoutPrent(root);
+                    console.log(JSON.stringify(rootClone, null, 4));
+                }
+                else {
+                    console.log(st.prettyPrint(root, Boolean(process.stdout.isTTY)));
+                }
             }
         }
         catch (err) {
-            console.error('failed to open the file', filePath);
+            console.error('failed to open the file', filePath, err);
         }
     }
 }
--- bread.ts
+++ bread.ts
@@ -6,11 +6,33 @@ const args = process.argv.slice(2);
 
 if(args.length ===0 || args[0] === '-h'){
     console.log('parses the files passed to arguments and prints tree')
-    console.log('-h : prints this message')
+    console.log('-h     : prints this message')
+    console.log('--json : print as json')
     process.exit(0);
 }
 
+class ItemWithoutPrent{
+    attributes: Array<string> = [];
+    body: Array<string | ItemWithoutPrent> = [];
+
+    constructor(item : st.Item){
+        this.attributes = item.attributes;
+        for(const child of item.body){
+            if(typeof child === 'string'){
+                this.body.push(child)
+            }
+            else{
+                let cloneChild = new ItemWithoutPrent(child);
+                this.body.push(cloneChild);
+            }
+        }
+    }
+}
+
 for(const filePath of args){
+    if(filePath === '--json' || filePath === '-h' ){
+        continue;
+    }
     try{
         let content = fs.readFileSync(filePath, 'utf-8');
         let tokenizer = new st.Tokenizer(content, filePath);
@@ -21,10 +43,16 @@ for(const filePath of args){
             console.error(errorMsg);
         }
         else{
-            console.log(st.prettyPrint(root, Boolean(process.stdout.isTTY)));
+            if(args.includes('--json')){
+                let rootClone = new ItemWithoutPrent(root);
+                console.log(JSON.stringify(rootClone, null, 4))
+            }
+            else{
+                console.log(st.prettyPrint(root, Boolean(process.stdout.isTTY)));
+            }
         }
     }
     catch(err){
-        console.error('failed to open the file', filePath);
+        console.error('failed to open the file', filePath, err);
     }
 }
